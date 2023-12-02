ee0e43da95a026ee6950cf55ce5c4533b8228d65
Eludreen <152428074+Eludreen@users.noreply.github.com>
2023-12-02 11:19:18 +0000

    Add files via upload

---
+++ D1/Program.cs
@@ -0,0 +1,276 @@
+﻿using System.Globalization;
+using System.IO;
+ 
+static int lineCheck(string path)
+{
+    int lineCount = 0;
+    //detects the number of lines
+    using (StreamReader r = new StreamReader(path))
+    {
+        lineCount = 0;
+        while (r.ReadLine() != null)
+        {
+            lineCount++;
+        }
+    }
+    return lineCount;
+}
+static bool intCheck(string testing)
+{
+    int binner = 0;
+    bool isInt = false;
+    isInt = int.TryParse(testing, out binner);
+    return isInt;
+}
+ 
+ 
+static int readTotal()
+{
+ 
+    int total = 0;
+    string row_count = " ";
+    int num_found = 0;
+    string path = "Q1_input.txt";
+    int lineCount = lineCheck(path);
+    string transferer = " ";
+    using (StreamReader r = new StreamReader(path))
+    {
+        for (int i = 0; i < lineCount; i++)
+        {
+            row_count = null;
+            num_found = 0;
+            transferer = null;
+            string line = r.ReadLine();
+            for (int x = 0; x < line.Length; x++)
+            {
+                string int_check = (line[x]).ToString();
+                if (intCheck(int_check))
+                {
+                    num_found++;
+                    if (num_found == 1)
+                    {
+                        row_count += int_check;
+                    }
+                    else
+                    {
+                        transferer = int_check;
+                    }
+                }
+            }
+            if (num_found > 1)
+            {
+                row_count += transferer;
+            }
+            else
+            {
+                row_count += row_count;
+            }
+            Console.WriteLine(row_count);
+            total += (int.Parse(row_count));
+ 
+        }
+    }
+    Console.WriteLine(total);
+    return total;
+}
+ 
+static int ReadTotal2()
+{
+ 
+    int total = 0;
+    string row_count = " ";
+    string path = "Q1_input.txt";
+    int lineCount = lineCheck(path);
+    string transferer = " ";
+    string Digit5 = "";
+    string Digit4 = "";
+    string Digit3 = "";
+ 
+    using (StreamReader r = new StreamReader(path))
+    {
+        for (int i = 0; i < lineCount; i++)
+        {
+            row_count = "";
+            transferer = "";
+            string line = r.ReadLine();
+            int num_found = 0;
+            int leng = line.Length;
+            for (int x = 0; x < leng; x++)
+            {
+                string int_check = (line[x]).ToString();
+                bool isInt = intCheck(int_check);
+                if (isInt)
+                {
+                    num_found += 1;
+                    if (num_found == 1)
+                    {
+                        Console.WriteLine(num_found);
+                        row_count += int_check;
+                    }
+                    else
+                    {
+                        transferer = int_check;
+                    }
+                }
+                else
+                {
+                    if ((x + 4) < leng)
+                    {
+                        Digit5 = line.Substring(x, 5);
+                        Digit4 = line.Substring(x, 4);
+                        Digit3 = line.Substring(x, 3);
+                    }
+                    else if ((x + 3) < leng)
+                    {
+                        Digit4 = line.Substring(x, 4);
+                        Digit3 = line.Substring(x, 3);
+                        Digit5 = "";
+                    }
+                    else if ((x + 2) < leng)
+                    {
+                        Digit3 = line.Substring(x, 3);
+                        Digit5 = "";
+                        Digit4 = "";
+                    }
+ 
+                    switch (Digit5)
+                    {
+                        case ("three"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "3";
+                            }
+                            else
+                            {
+                                transferer = "3";
+                            }
+                            break;
+                        case ("seven"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "7";
+                            }
+                            else
+                            {
+                                transferer = "7";
+                            }
+                            break;
+                        case ("eight"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "8";
+                            }
+                            else
+                            {
+                                transferer = "8";
+                            }
+                            break;
+                    }
+                    switch (Digit4)
+                    {
+                        case ("four"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "4";
+                            }
+                            else
+                            {
+                                transferer = "4";
+                            }
+                            break;
+                        case ("five"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "5";
+                            }
+                            else
+                            {
+                                transferer = "5";
+                            }
+                            break;
+                        case ("nine"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "9";
+                            }
+                            else
+                            {
+                                transferer = "9";
+                            }
+                            break;
+ 
+                    }
+                    switch (Digit3)
+                    {
+                        case ("one"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "1";
+                            }
+                            else
+                            {
+                                transferer = "1";
+                            }
+                            break;
+                        case ("two"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "2";
+                            }
+                            else
+                            {
+                                transferer = "2";
+                            }
+                            break;
+                        case ("six"):
+                            num_found += 1;
+                            if (num_found == 1)
+                            {
+                                row_count += "6";
+                            }
+                            else
+                            {
+                                transferer = "6";
+                            }
+                            break;
+ 
+                    }
+                }
+ 
+            }
+            if (num_found > 1)
+            {
+                row_count += transferer;
+            }
+            else
+            {
+                row_count += row_count;
+            }
+            Console.WriteLine(row_count);
+            total += (int.Parse(row_count));
+            num_found = 0;
+        }
+    }
+    return total;
+}
+Console.WriteLine("D1: Q1 or 2?");
+string num = Console.ReadLine();
+int total = 0;
+if (num == "1")
+{
+     total = readTotal();
+}
+else if (num == "2")
+{
+     total = ReadTotal2();
+}
+ 
+Console.WriteLine(total);
---
+++ D2/Program.cs
@@ -0,0 +1,221 @@
+﻿using System.Diagnostics;
+
+string path = "D2_input.txt";
+static bool intCheck(string toCheck)
+{
+    int binner = 0;
+    bool IsInt = false;
+    IsInt = int.TryParse(toCheck, out binner);
+    return IsInt;
+}
+static int LineCount(string path)
+{
+    int lineCount = 0;
+    using (StreamReader r = new StreamReader(path))
+    {
+        while (r.ReadLine() != null)
+        {
+            lineCount++;
+        }
+    }
+    return(lineCount);  
+}
+static bool Allow(string colour, int count)
+{
+    if (colour == "g")
+    {
+        if (count <= 13)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    if (colour == "r")
+    {
+        if (count <= 12)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    if (colour == "b")
+    {
+        if (count <= 14)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return false;
+}
+int total_power = 0;
+int greenCount = 0;
+int redCount = 0;
+int blueCount = 0;
+int gameNum = 0;
+
+string digit5 = "";
+string digit4 = "";
+string digit3 = "";
+int lineCount = LineCount(path);
+using (var sr = new StreamReader(path))
+{
+    for (int i = 0; i < lineCount; i++)
+    {
+        int bigGreen = 0, bigBlue = 0, bigRed = 0;
+        Console.WriteLine();
+        bool greenAllow = true;
+        bool blueAllow = true;
+        bool redAllow = true;
+        greenCount = 0;
+        redCount = 0;
+        blueCount = 0;
+        string line = sr.ReadLine();
+        line = line.Remove(0, 5);
+        for (int x = 0; x < line.Length; x++)
+        {
+            bool isLetter = char.IsLetter(line[x]);
+            if (isLetter)
+            {
+                if (x + 4 < line.Length)
+                {
+                     digit5 = line.Substring(x, 5);
+                     digit4 = line.Substring(x, 4);
+                     digit3 = line.Substring(x, 3);
+                }
+                else if (x + 3 < line.Length)
+                {
+                    digit5 = "";
+                    digit4 = line.Substring(x, 4);
+                    digit3 = line.Substring(x, 3);
+                }
+                else if (x + 2 < line.Length)
+                {
+                    digit5 = "";
+                    digit4 = "";
+                    digit3 = line.Substring(x, 3);
+                }
+                else
+                {
+                    digit5 = "";
+                    digit4 = "";
+                    digit3 = "";
+                }
+
+
+                if (digit5 == "green")
+                {
+                    bool ints2 = intCheck(line[x - 3].ToString());
+                    if (ints2)
+                    {
+                        string transfer = ((line[x - 3].ToString()) + (line[x - 2].ToString()));
+                        greenCount = int.Parse(transfer);
+                    }
+                    else
+                    {
+                        greenCount = int.Parse(line[x - 2].ToString());
+                    }
+                    if (bigGreen < greenCount)
+                    {
+                        bigGreen = greenCount;
+                    }
+                    if (greenAllow)
+                    {
+                        greenAllow = Allow("g", greenCount);
+                    }
+                }
+                if (digit4 == "blue")
+                {
+                    bool ints2 =  intCheck(line[x - 3].ToString());
+                    if (ints2)
+                    {
+                        string transfer = ((line[x - 3].ToString()) + (line[x - 2].ToString()));
+                        blueCount = int.Parse(transfer);
+                    }
+                    else
+                    {
+                        blueCount = int.Parse(line[x - 2].ToString());
+                    }
+                    if (bigBlue < blueCount)
+                    {
+                        bigBlue = blueCount;
+                    }
+                    if (blueAllow)
+                    {
+                        blueAllow = Allow("b", blueCount);
+                    }
+
+                }
+                if (digit3 == "red")
+                {
+                    bool ints2 =  intCheck(line[x - 3].ToString());
+                    if (ints2)
+                    {
+                        string transfer = ((line[x - 3].ToString())+(line[x - 2].ToString()));
+                        redCount = int.Parse(transfer);
+                    } 
+                    else
+                    {
+                        redCount = int.Parse(line[x - 2].ToString());
+                    }
+                    if (bigRed < redCount)
+                    {
+                        bigRed = redCount;
+                    }
+                    if (redAllow)
+                    {
+                        redAllow = Allow("r", redCount);
+                    }
+
+                }
+
+
+            }
+
+        }
+        int powerTrans = (bigBlue * bigRed * bigGreen);
+        Console.WriteLine(powerTrans);
+        total_power += powerTrans;
+        if ((redAllow) && (blueAllow) && (greenAllow))
+        {
+
+            string transfer = "";
+            int first = 0;
+            bool intsis = intCheck(line[0].ToString());
+            if (intsis)
+            {
+                bool intsis2 = intCheck(line[1].ToString());
+                if (intsis2)
+                {
+                    bool intsis3 = intCheck(line[2].ToString());
+                    if (intsis3)
+                    {
+                         transfer = ((line[0].ToString()) + (line[1].ToString()) + (line[2].ToString()));
+                    }
+                    else
+                    {
+                        transfer = ((line[0].ToString()) + (line[1].ToString()));
+                    }
+                }
+                else
+                {
+                    transfer = (line[0].ToString());
+                }
+
+            }
+            first = int.Parse(transfer);
+            gameNum += first;
+        }
+    }
+}
+
+Console.WriteLine(total_power);
